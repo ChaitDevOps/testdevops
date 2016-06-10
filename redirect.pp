b33eafc247009a5dd401ea8dce607b915d6cd34b
Chaitanya DevOps <cbingu@gmail.com>
2016-06-09 22:13:48 -0400

    Made few Adjustements

--- redirect.pp
+++ redirect.pp
@@ -1,9 +1,13 @@
-  file { '/etc/httpd/conf.d/index.conf':
+class redirect {
+
+file { '/etc/httpd/conf.d/index.conf':
   	ensure  => present,
     notify  => Service['httpd'],  
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
-  }
+  }	
 
 }
+
+include redirect
